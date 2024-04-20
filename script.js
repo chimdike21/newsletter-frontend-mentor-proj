c18e3598e4edfd3698630999504a76ee419823ea
chimdike21 <138835446+chimdike21@users.noreply.github.com>
2024-04-20 15:00:43 +0100

    modifications

--- script.js
+++ script.js
@@ -15,4 +15,28 @@ document.addEventListener("DOMContentLoaded", function(){
             newsletterForm.style.display="none"
         }
     } )
+
+    window.dismissSignupSuccess= function(){
+        if(signupSuccess){
+            signupSuccess.style.display='none'
+        }
+
+        const signin= document.getElementById('signin')
+        if(signin){
+            signin.style.display='block'
+        }
+    }
+    document.getElementById('email').addEventListener('input', function(){
+        var emailInput= this.value
+        var emailRegex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+        if (emailRegex.test(emailInput)){
+            document.getElementById('error-alert').style.display='none'
+        }else{
+            document.getElementById('error-alert').style.display='block'
+            document.getElementById('email').style.backgroundColor='hsl(0, 100%,96%)'
+            document.getElementById('email').style.color='hsl(4, 100%, 67%)'
+            document.getElementById('email').style.border='1px solid hsl(4, 100%, 67%'
+        }
+    })
 })
